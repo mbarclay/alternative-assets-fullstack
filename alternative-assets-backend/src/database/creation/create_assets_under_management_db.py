7aa1167a033b788e8304932353b88514ee2f960a
Malcolm Barclay <malcolm@mbarclay.net>
2024-08-13 18:45:22 +0100

    [WIP] placeholders for data population

--- alternative-assets-backend/src/database/creation/create_assets_under_management_db.py
+++ alternative-assets-backend/src/database/creation/create_assets_under_management_db.py
@@ -1,7 +1,10 @@
 import os
 import sqlite3
 
+import pandas as pd
+
 from config import settings
+from src.database.connection import Database
 
 
 def create_database(sql_path: str, db_path: str):
@@ -22,6 +25,45 @@ def create_database(sql_path: str, db_path: str):
         conn.close()
 
 
+def populate_database():
+    session = Database.get_session()
+
+    # read our data csv into a pandas data frame
+    df = pd.read_csv("data.csv")
+
+    # insert our various entities
+    insert_investory_types(df)
+    insert_countries(df)
+    insert_asset_classes(df)
+    insert_investors(df)
+
+    session.close()
+
+
+def column_to_unique_list(df: pd.DataFrame, column: str):
+    return df[column].unique().tolist()
+
+
+def insert_investory_types(df: pd.DataFrame):
+    investory_types = column_to_unique_list(df, "Investory Type")
+    print(investory_types)
+
+
+def insert_countries(df: pd.DataFrame):
+    countries = column_to_unique_list(df, "Investor Country")
+    print(countries)
+
+
+def insert_asset_classes(df: pd.DataFrame):
+    assets_classes = column_to_unique_list(df, "Commitment Asset Class")
+    print(assets_classes)
+
+
+def insert_investors(df: pd.DataFrame):
+    investors = column_to_unique_list(df, "Investor Name")
+    print(investors)
+
+
 if __name__ == "__main__":
     # define the sql file path and the database name
     sql = "assets_under_management.sql"
@@ -33,3 +75,6 @@ if __name__ == "__main__":
 
     # create the database
     create_database(sql, db)
+
+    # populate the database
+    populate_database()
