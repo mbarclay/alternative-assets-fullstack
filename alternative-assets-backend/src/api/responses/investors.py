d7a5f8e3f72056ed20b0669ae529c54bfb84276d
Malcolm Barclay <malcolm@mbarclay.net>
2024-08-15 07:58:33 +0100

    [ADD] total commitment and other properties

--- alternative-assets-backend/src/api/responses/investors.py
+++ alternative-assets-backend/src/api/responses/investors.py
@@ -8,6 +8,9 @@ class InvestorResponse(BaseModel):
     name: str
     country_iso: str
     country_name: str
+    investory_type_code: str
+    investory_type: str
+    total_commitment: int
 
     model_config = {
         "from_attributes": True
--- alternative-assets-backend/src/main.py
+++ alternative-assets-backend/src/main.py
@@ -2,11 +2,13 @@ from typing import List
 
 import uvicorn
 from fastapi import FastAPI, Request
-from sqlalchemy.orm import joinedload
+from sqlalchemy import func
+from sqlalchemy.dialects import sqlite
+from sqlalchemy.orm import joinedload, subqueryload
 
 from src.api.responses.investors import InvestorResponse
 from src.database.connection import Database
-from src.model.assets_under_management import Investor
+from src.model.assets_under_management import Investor, Commitment
 
 app = FastAPI()
 
@@ -19,20 +21,42 @@ async def home(request: Request):
 @app.get("/investors", response_model=List[InvestorResponse])
 async def home(request: Request):
     session = Database.get_session()
-    investors = session.query(Investor).options(joinedload(Investor.country)).all()
+
+    # use a subquery to calculate the total commitment for each investor
+    subquery = session.query(
+        Commitment.investor_code,
+        func.sum(Commitment.amount).label('total_commitment')
+    ).group_by(Commitment.investor_code).subquery()
+
+    # query investors, subquery for their commitments totals
+    investors = (session.query(
+        Investor,
+        subquery.c.total_commitment
+    ).outerjoin(
+        subquery, Investor.investor_code == subquery.c.investor_code
+    ).options(
+        subqueryload(Investor.country)
+    ).options(
+        subqueryload(Investor.investory_type)
+    ).all())
+
     session.close()
 
-    investor_responses = [
+    # convert sqlalchemy to our pydantic response
+    investors_response = [
         InvestorResponse(
-            investor_code=inv.investor_code,
-            name=inv.name,
-            country_iso=inv.country.iso,
-            country_name=inv.country.name
+            investor_code=investor.investor_code,
+            name=investor.name,
+            country_iso=investor.country.iso if investor.country else None,
+            country_name=investor.country.name if investor.country else None,
+            investory_type_code=investor.investory_type_code,
+            investory_type=investor.investory_type.investory_type if investor.investory_type else None,
+            total_commitment=total_commitment if total_commitment else 0
         )
-        for inv in investors
+        for investor, total_commitment in investors
     ]
 
-    return investor_responses
+    return investors_response
 
 
 if __name__ == "__main__":
