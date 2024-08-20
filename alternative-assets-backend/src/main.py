bb4070206a9b42a7a06c727086746ce758f4a36a
Malcolm Barclay <malcolm@mbarclay.net>
2024-08-20 08:03:53 +0100

    [NIT] tidy ups

--- alternative-assets-backend/src/main.py
+++ alternative-assets-backend/src/main.py
@@ -46,4 +46,4 @@ async def investor_commitments(investor_code: str, asset_class_code: str) -> Lis
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
